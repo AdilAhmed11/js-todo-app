d18b35399945b8cd76184665a6ff66093a1e3668
Adil Ahmed <adilahmed749@gmail.com>
2025-03-19 12:24:31 +0000

    Refactoring the closeButtons variable into a function called registerEventListeners and aaded it to the newTodo function. Deleted JSON data.

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
-    var json_data = [
-       {
-         "userId": 1,
-         "id": 1,
-         "title": "delectus aut autem",
-         "completed": false
-       },
-       {
-         "userId": 1,
-         "id": 2,
-         "title": "quis ut nam facilis et officia qui",
-         "completed": false
-       },
-       {
-         "userId": 1,
-         "id": 3,
-         "title": "fugiat veniam minus",
-         "completed": false
-       },
-       {
-         "userId": 1,
-         "id": 4,
-         "title": "et porro tempora",
-         "completed": true
-       },
-       {
-         "userId": 1,
-         "id": 5,
-         "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-         "completed": false
-       }
-     ];
+    var json_data = [];
 
     var myList = document.getElementById("todo-list");
  
     json_data.forEach(element => {
        newTodo(element.title);
     });
-    var closeButtons = document.getElementsByClassName("delete"); //Grabs every delete button
-    for (var i = 0; i < closeButtons.length; i++) { //Loops through every delete button
-        closeButtons[i].addEventListener('click', deleteTodo, false); //Adds event listener to each button, and the deleteTodo function
+    registerEventListeners();
+    function registerEventListeners() {
+      var closeButtons = document.getElementsByClassName("delete"); //Grabs every delete button
+      for (var i = 0; i < closeButtons.length; i++) { //Loops through every delete button
+          closeButtons[i].addEventListener('click', deleteTodo, false); //Adds event listener to each button, and the deleteTodo function
+      }
     }
     function deleteTodo() {
         var li = this.parentElement; //Grabs the parent list element (the text and delete button are children)
         myList.removeChild(li);
       }
-    function newTodo() {
-       if (!todoTitle) {
+    function newTodo(todoTitle) {
+       if (!todoTitle) { //If not empty then do this
         todoTitle = document.getElementById("todoTitle").value;
        }
        var listItem = document.createElement("li");
@@ -60,4 +32,5 @@
        );
        listItem.appendChild(deleteLink);
        myList.appendChild(listItem);
+       registerEventListeners();
      }
