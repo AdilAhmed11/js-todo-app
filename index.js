e9134b9247f6468f35a8d6b3fce910e7c0cd20a2
Adil Ahmed <adilahmed749@gmail.com>
2025-03-19 11:46:46 +0000

    Refactoring code

--- index.js
+++ index.js
@@ -31,39 +31,26 @@
        }
      ];
 
-     var myList = document.getElementById("todo-list");
+    var myList = document.getElementById("todo-list");
  
-     json_data.forEach(element => {
-       var listItem = document.createElement("li");
-       listItem.appendChild(document.createTextNode(element.title)
-    );
-    var deleteLink = document.createElement("a");
-
-    deleteLink.href = "#"; //Presses the button without opening a new page
-    deleteLink.className = "btn btn-sm btn-danger m-1 delete";
-    deleteLink.appendChild(
-        document.createTextNode("Delete") //Adding delete text to the button
-    );
-    listItem.appendChild(deleteLink);
-    myList.appendChild(listItem);
-     });
-
+    json_data.forEach(element => {
+       newTodo(element.title);
+    });
     var closeButtons = document.getElementsByClassName("delete"); //Grabs every delete button
- 
-     for (var i = 0; i < closeButtons.length; i++) { //Loops through every delete button
-       closeButtons[i].addEventListener('click', deleteTodo, false); //Adds event listener to each button, and the deleteTodo function
-     }
- 
-     function deleteTodo() {
-       var li = this.parentElement; //Grabs the parent list element (the text and delete button are children)
-       myList.removeChild(li);
-     }
-
+    for (var i = 0; i < closeButtons.length; i++) { //Loops through every delete button
+        closeButtons[i].addEventListener('click', deleteTodo, false); //Adds event listener to each button, and the deleteTodo function
+    }
+    function deleteTodo() {
+        var li = this.parentElement; //Grabs the parent list element (the text and delete button are children)
+        myList.removeChild(li);
+      }
     function newTodo() {
-       var inputVal = document.getElementById("todoTitle").value;
+       if (!todoTitle) {
+        todoTitle = document.getElementById("todoTitle").value;
+       }
        var listItem = document.createElement("li");
        listItem.appendChild(
-         document.createTextNode(inputVal) 
+         document.createTextNode(todoTitle) 
        );
        var deleteLink = document.createElement("a");
        deleteLink.href = "#";
